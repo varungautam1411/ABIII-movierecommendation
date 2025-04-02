9c99ed18c6b65213dcdde70672667c657db0abdf
varungautam1411 <archervg@gmail.com>
2025-04-02 10:52:13 -0700

    Update LambdaHandler.java

--- src/main/java/com/example/movieratingservice/handler/LambdaHandler.java
+++ src/main/java/com/example/movieratingservice/handler/LambdaHandler.java
@@ -1,1 +1,33 @@
+package com.example.movieratingservice.handler;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.example.movieratingservice.MovieRatingServiceApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class LambdaHandler implements RequestStreamHandler {
+
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(MovieRatingServiceApplication.class);
+        } catch (ContainerInitializationException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
 
