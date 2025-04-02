a3aa1fb9b79a2d0295edf4b2640f8e7db78974c2
varungautam1411 <archervg@gmail.com>
2025-04-02 10:50:21 -0700

    Update MemoryDBConfig.java

--- src/main/java/com/example/movieratingservice/config/MemoryDBConfig.java
+++ src/main/java/com/example/movieratingservice/config/MemoryDBConfig.java
@@ -1,1 +1,38 @@
+package com.example.movieratingservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class MemoryDBConfig {
+
+    @Value("${memorydb.host}")
+    private String host;
+
+    @Value("${memorydb.port}")
+    private int port;
+
+    @Value("${memorydb.password}")
+    private String password;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
