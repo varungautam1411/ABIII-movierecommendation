68406144bb9bf186ec8c4f790b3638cf086f803f
varungautam1411 <archervg@gmail.com>
2025-04-02 10:58:57 -0700

    Update LambdaHandler.java

--- src/main/java/com/example/movieratingservice/handler/LambdaHandler.java
+++ src/main/java/com/example/movieratingservice/handler/LambdaHandler.java
@@ -7,27 +7,36 @@ import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.example.movieratingservice.MovieRatingServiceApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class LambdaHandler implements RequestStreamHandler {
-
+    private static final Logger logger = LoggerFactory.getLogger(LambdaHandler.class);
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(MovieRatingServiceApplication.class);
+            handler = SpringBootLambdaContainerHandler
+                    .getAwsProxyHandler(MovieRatingServiceApplication.class);
+            
+            // If you need to modify the handler settings:
+            handler.getContainerConfig().setInitializationTimeout(50_000);
+            handler.getContainerConfig().setDefaultContentCharset("UTF-8");
+            
         } catch (ContainerInitializationException e) {
-            e.printStackTrace();
+            logger.error("Could not initialize Spring Boot application", e);
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) 
+            throws IOException {
         handler.proxyStream(inputStream, outputStream, context);
+        outputStream.close();
     }
 }
-
