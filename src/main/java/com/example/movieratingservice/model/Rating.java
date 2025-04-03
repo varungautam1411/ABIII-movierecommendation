62c07733b76986e831b0d01f85fde4529174722c
varungautam1411 <archervg@gmail.com>
2025-04-03 16:23:00 -0700

    Update Rating.java

--- src/main/java/com/example/movieratingservice/model/Rating.java
+++ src/main/java/com/example/movieratingservice/model/Rating.java
@@ -1,4 +1,64 @@
- 
 package com.example.movieratingservice.model;
 
-public record Rating(String customerId, String movieId, int rating, String date) {}
+import java.io.Serializable;
+
+public class Rating implements Serializable {
+    private String customerId;
+    private String movieId;
+    private int rating;
+    private String date;
+
+    // Default constructor
+    public Rating() {}
+
+    // Constructor with all fields
+    public Rating(String customerId, String movieId, int rating, String date) {
+        this.customerId = customerId;
+        this.movieId = movieId;
+        this.rating = rating;
+        this.date = date;
+    }
+
+    // Getters and Setters
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "customerId='" + customerId + '\'' +
+                ", movieId='" + movieId + '\'' +
+                ", rating=" + rating +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
