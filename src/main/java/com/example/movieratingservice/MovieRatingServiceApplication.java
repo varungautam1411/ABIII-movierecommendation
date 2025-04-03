c7abfdf27d848dc845e736caea92f2d537450d60
varungautam1411 <archervg@gmail.com>
2025-04-03 16:23:37 -0700

    Update MovieRatingServiceApplication.java

--- src/main/java/com/example/movieratingservice/MovieRatingServiceApplication.java
+++ src/main/java/com/example/movieratingservice/MovieRatingServiceApplication.java
@@ -1,11 +1,26 @@
 package com.example.movieratingservice;
 
+import com.example.movieratingservice.model.Rating;
+import com.example.movieratingservice.service.RatingService;
+
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MovieRatingServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MovieRatingServiceApplication.class, args);
+    }
+
+    @Bean
+    public Function<Rating, String> addRating(RatingService ratingService) {
+        return rating -> {
+            ratingService.addRating(rating);
+            return "Rating added successfully";
+        };
     }
 }
