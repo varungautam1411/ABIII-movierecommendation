ac11517da62640869bbfb3beee7cbe1937099933
varungautam1411 <archervg@gmail.com>
2025-04-03 16:22:43 -0700

    Update LambdaHandler.java

--- src/main/java/com/example/movieratingservice/handler/LambdaHandler.java
+++ src/main/java/com/example/movieratingservice/handler/LambdaHandler.java
@@ -1,42 +1,12 @@
 package com.example.movieratingservice.handler;
 
-import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.example.movieratingservice.MovieRatingServiceApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.example.movieratingservice.model.Rating;
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public class LambdaHandler implements RequestStreamHandler {
-    private static final Logger logger = LoggerFactory.getLogger(LambdaHandler.class);
-    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-
-    static {
-        try {
-            handler = SpringBootLambdaContainerHandler
-                    .getAwsProxyHandler(MovieRatingServiceApplication.class);
-            
-            // If you need to modify the handler settings:
-            handler.getContainerConfig().setInitializationTimeout(50_000);
-            handler.getContainerConfig().setDefaultContentCharset("UTF-8");
-            
-        } catch (ContainerInitializationException e) {
-            logger.error("Could not initialize Spring Boot application", e);
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
-    }
-
-    @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) 
-            throws IOException {
-        handler.proxyStream(inputStream, outputStream, context);
-        outputStream.close();
-    }
+@SuppressWarnings({ "deprecation", "unused" })
+public class LambdaHandler extends SpringBootRequestHandler<Rating, String> {
 }
+
+    
