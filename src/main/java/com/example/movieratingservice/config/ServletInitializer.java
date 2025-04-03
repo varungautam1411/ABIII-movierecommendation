895463e82f5ccbd09df62be0c0ce16cb0f3e7c36
varungautam1411 <archervg@gmail.com>
2025-04-03 16:21:34 -0700

    Update ServletInitializer.java

--- src/main/java/com/example/movieratingservice/config/ServletInitializer.java
+++ src/main/java/com/example/movieratingservice/config/ServletInitializer.java
@@ -1,26 +1,15 @@
 package com.example.movieratingservice.config;
 
-import com.amazonaws.serverless.proxy.spring.SpringLambdaContainerHandler;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 import com.example.movieratingservice.MovieRatingServiceApplication;
 
 @Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
     
     @Override
-    protected WebApplicationContext createRootApplicationContext() {
-        WebApplicationContext context = super.createRootApplicationContext();
-        // Add Lambda response flush listener
-        DispatcherServlet dispatcherServlet = context.getBean(DispatcherServlet.class);
-        dispatcherServlet.addListener(new SpringLambdaContainerHandler.LambdaFlushResponseListener());
-        return context;
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] { MovieRatingServiceApplication.class };
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MovieRatingServiceApplication.class);
     }
 }
